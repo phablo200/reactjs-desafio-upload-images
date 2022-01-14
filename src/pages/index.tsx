98cad5a013e161a5789d45d93845bcc6b749505d
Phablo Vilas Boas <phablo_sena@hotmail.com>
2022-01-14 05:32:24 -0300

    finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,7 @@ export default function Home(): JSX.Element {
     fetchNextPage,
     hasNextPage,
   } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+    'images');
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
@@ -38,6 +34,9 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {isLoading && (<Loading />)}
+        {isError && (<Error />)}
       </Box>
     </>
   );
